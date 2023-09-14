031885398c6e30b182869a3f3511ce81ebaa1c84
Jyods <jyodi.sprenger@hotmail.ch>
2023-09-14 14:48:31 +0200

    page now loading

--- dist/assets/BlogView-c973efa1.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index-d30e94a2.js";const r={__name:"BlogView",setup(t){return e(()=>{document.title="To be implemented | Jyods Systems"}),(o,m)=>" To be implemented "}};export{r as default};
---
+++ dist/assets/ContactView-b5a6529b.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index-74c83b75.js";const r={__name:"ContactView",setup(t){return e(()=>{document.title="To be implemented | Jyods Systems"}),(o,n)=>" To be implemented "}};export{r as default};
