44e91498dcd6576626b614cf747dba2348e03e55
Jyods <jyodi.sprenger@hotmail.ch>
2023-09-14 13:43:41 +0200

    changed to hash mode

---
+++ dist/assets/ContactView-cee36689.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index-d30e94a2.js";const r={__name:"ContactView",setup(t){return e(()=>{document.title="To be implemented | Jyods Systems"}),(o,n)=>" To be implemented "}};export{r as default};
