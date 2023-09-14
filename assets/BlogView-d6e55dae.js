832b05d1f5dbaa9c965af7737baaad3b5c167661
Jyods <jyodi.sprenger@hotmail.ch>
2023-09-14 13:34:55 +0200

    added new Page Version

--- assets/BlogView-d6e55dae.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-718e415d.js";const r={};function t(c,n){return" To be implemented "}const _=e(r,[["render",t]]);export{_ as default};
--- assets/ContactView-418f262e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-718e415d.js";const t={};function c(n,r){return" To be implemented "}const _=e(t,[["render",c]]);export{_ as default};
