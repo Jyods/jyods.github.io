df730f0c2c8f06b2e84885b87267c5a6209aa4e4
Jyods <jyodi.sprenger@hotmail.ch>
2023-09-15 16:46:30 +0200

    added copyright

---
+++ assets/BlogView-65d3f8f1.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-d05e501b.js";const r={__name:"BlogView",setup(t){return e(()=>{document.title="To be implemented | Jyods Systems"}),(o,m)=>" To be implemented "}};export{r as default};
