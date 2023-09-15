f94aa4f2a4983d4c16a37ef5cf49f890338f3ac0
Jyods <jyodi.sprenger@hotmail.ch>
2023-09-15 16:03:49 +0200

    added commissions

---
+++ assets/BlogView-5663616f.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-edfe22e5.js";const r={__name:"BlogView",setup(t){return e(()=>{document.title="To be implemented | Jyods Systems"}),(o,m)=>" To be implemented "}};export{r as default};
---
+++ assets/ImpressView-81404046.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c}from"./index-edfe22e5.js";const r={},o={class:"impress_wrapper"};function t(_,a){return s(),c("div",o," Hallo ")}const p=e(r,[["render",t]]);export{p as default};
