b6da7243bdc3e3224e17d591fe3ac0ad38bde0e0
Jyods <jyodi.sprenger@hotmail.ch>
2023-09-15 17:29:40 +0200

    added responsivity

---
+++ assets/BlogView-6866aa39.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-b8f60f6b.js";const r={__name:"BlogView",setup(t){return e(()=>{document.title="To be implemented | Jyods Systems"}),(o,m)=>" To be implemented "}};export{r as default};
---
+++ assets/ImpressView-a68e4b65.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c}from"./index-b8f60f6b.js";const r={},o={class:"impress_wrapper"};function t(_,a){return s(),c("div",o," Hallo ")}const p=e(r,[["render",t]]);export{p as default};
