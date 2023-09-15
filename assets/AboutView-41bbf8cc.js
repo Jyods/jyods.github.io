01659240588a0151f75160361dd823a910351e27
Jyods <jyodi.sprenger@hotmail.ch>
2023-09-15 14:58:06 +0200

    added v1

--- assets/AboutView-41bbf8cc.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index-74c83b75.js";const r={__name:"AboutView",setup(t){return e(()=>{document.title="To be implemented | Jyods Systems"}),(o,m)=>" To be implemented "}};export{r as default};
