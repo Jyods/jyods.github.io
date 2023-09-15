84bad47ec7e1bee2f4f2edef59743d6f284cef7e
Jyods <jyodi.sprenger@hotmail.ch>
2023-09-15 15:12:02 +0200

    changed burger blog to contact

--- assets/BlogView-ce1c8985.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./index-51e0ac04.js";const r={__name:"BlogView",setup(t){return e(()=>{document.title="To be implemented | Jyods Systems"}),(o,m)=>" To be implemented "}};export{r as default};
