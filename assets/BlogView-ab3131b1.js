304f3b3cad5144a0dd9800afae7600bf98f11f8d
Jyods <jyodi.sprenger@hotmail.ch>
2023-09-15 15:37:33 +0200

    changed title some style

---
+++ assets/BlogView-ab3131b1.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-fb759d31.js";const r={__name:"BlogView",setup(t){return e(()=>{document.title="To be implemented | Jyods Systems"}),(o,m)=>" To be implemented "}};export{r as default};
