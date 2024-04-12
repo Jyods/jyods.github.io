e4566f11c9a0ade6cd154444ce26d67211e5ce98
Jyods <Jyodi.Sprenger@hotmail.ch>
2024-04-13 01:34:25 +0200

    added new version

---
+++ assets/BlogView-9626ddc5.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-5e04364f.js";const r={__name:"BlogView",setup(t){return e(()=>{document.title="To be implemented | Jyods Systems"}),(o,m)=>" To be implemented "}};export{r as default};
