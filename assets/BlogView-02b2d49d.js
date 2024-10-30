0e5149f4c99f201c754344a8d7123fb956a711e3
Jyods <jyodi.sprenger@hotmail.ch>
2024-10-30 08:43:19 +0100

    changed year to 2024

--- assets/BlogView-02b2d49d.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./index-260e7b2a.js";const r={__name:"BlogView",setup(t){return e(()=>{document.title="To be implemented | Jyods Systems"}),(o,m)=>" To be implemented "}};export{r as default};
