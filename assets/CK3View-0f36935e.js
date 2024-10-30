8f13ff33f8d8ae2fa2c62bdd20ec698ce6d5387b
Jyods <jyodi.sprenger@hotmail.ch>
2024-10-30 10:23:22 +0100

    added cool button style

---
+++ assets/CK3View-0f36935e.js
@@ -0,0 +1,1 @@
+import{_ as e,d as t,o,c as _}from"./index-10358be8.js";const c={__name:"CK3View",setup(n){return t(()=>{document.title="Weiterleitung... | Jyods Systems",window.location.href="https://www.youtube.com/watch?v=19RuONRmI_8"}),(s,a)=>(o(),_("h1",null," Weiterleitung... "))}},u=e(c,[["__scopeId","data-v-b04693ca"]]);export{u as default};
