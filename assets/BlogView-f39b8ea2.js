28ab5265bdf10e20967efdd2af8e802c982771bd
Jyods <jyodi.sprenger@hotmail.ch>
2025-10-28 15:17:55 +0100

    added calendar

---
+++ assets/BlogView-f39b8ea2.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-1af11a9e.js";const r={__name:"BlogView",setup(t){return e(()=>{document.title="To be implemented | Jyods Systems"}),(o,m)=>" To be implemented "}};export{r as default};
